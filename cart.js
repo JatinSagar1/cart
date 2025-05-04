b04587fa701599e3850a51ce72d032c30c6ab01b
JatinSagar1 <150676293+JatinSagar1@users.noreply.github.com>
2025-05-04 22:00:08 +0530

    inc

--- cart.js
+++ cart.js
@@ -1,6 +1,15 @@
 let btns = document.querySelectorAll(".btn");
 let cc = document.querySelector(".cc");
 let plus = document.querySelector(".plus");
+let cart_img = document.querySelectorAll(".cart-img");
+let item = document.querySelectorAll(".item");
+let cdes = document.querySelectorAll(".cdes");
+let ides = document.querySelectorAll(".ides");
+let rup = document.querySelectorAll(".rup");
+let iprice = document.querySelectorAll(".iprice");
+let inc = document.querySelectorAll(".inc");
+let dec = document.querySelectorAll(".dec");
+let no = document.querySelectorAll(".no");
 
 let count = 0;
 
@@ -18,3 +27,26 @@ btns.forEach((e)=>{
             plus.style.visibility = "visible"; 
     })
 })
+
+// btns[1].addEventListener("click", ()=>{
+//    cart_img[1].src = item[1].src;
+//    cdes[1].textContent = ides[1].textContent;
+//    rup[1].textContent = iprice[1].textContent;
+//    no[1].textContent = count;
+// })
+
+dec.forEach(e => {
+    e.addEventListener("click", ()=>{
+        if(count > 0){
+           count--;
+           plus.textContent = `+${count}`;
+        }
+    })
+});
+
+inc.forEach(e => {
+    e.addEventListener("click", ()=>{
+        count++;
+        plus.textContent = `+${count}`;
+    })
+});
